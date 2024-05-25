779a277b893560640aa0a0770c6ef5599c6cf5c1
ishukeshri2712 <85843603+ishukeshri2712@users.noreply.github.com>
2024-05-25 22:54:15 +0530

    Create tagging.sh

---
+++ tagging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TAG_KEY="Environment"
+TAG_VALUE="Production"
+
+INSTANCES=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].[InstanceId]" --output text)
+
+for INSTANCE in $INSTANCES; do
+
+  TAGS=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" --query "Tags")
+
+  if [ "$TAGS" == "[]" ]; then
+    echo "Instance $INSTANCE has no tags. Tagging it with $TAG_KEY=$TAG_VALUE"
+    
+    aws ec2 create-tags \ --resources $INSTANCE \ --tags Key=$TAG_KEY,Value=$TAG_VALUE
+  else
+    echo "Instance $INSTANCE is already tagged."
+  fi
+done
+
+echo "Tagging complete."
